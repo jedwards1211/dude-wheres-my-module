4e9be5eb4447f1f73595748c3098ed1503c4d438
Andy Edwards <jedwards@fastmail.com>
2019-12-08 17:12:41 -0600

    fix: bug where connect fails before new server starts up

--- src/Client.js
+++ src/Client.js
@@ -6,15 +6,14 @@ import net from 'net'
 import tempFiles from './tempFiles'
 import { spawn } from 'child_process'
 import { promisify } from 'util'
-import fs from 'fs-extra'
-import delay from 'delay'
 import stream from 'stream'
 import JSONStream from 'JSONStream'
 import EventEmitter from '@jcoreio/typed-event-emitter'
 import findRoot from 'find-root'
 import { type SuggestMessage, type WheresMessage } from './Server'
-
 import { type SuggestedImportResult } from './ModuleIndex'
+import poll from '@jcoreio/poll'
+import emitted from 'p-event'
 
 export type Message = {
   seq: number,
@@ -26,6 +25,13 @@ type Events = {
   ready: [],
   progress: [Progress],
   starting: [],
+}
+
+async function connected(socket: net.Socket): Promise<void> {
+  await emitted(socket, 'connect', {
+    rejectionEvents: ['error', 'exit'],
+    timeout: 10000,
+  })
 }
 
 export default class Client extends EventEmitter<Events> {
@@ -46,133 +52,81 @@ export default class Client extends EventEmitter<Events> {
     this.outstream = new JSONStream.stringify()
   }
 
+  _createClient = async (): Promise<net.Socket> => {
+    const files = tempFiles(this.projectRoot)
+
+    let client: net.Socket
+    try {
+      // in case a server is already running, try to connect to the socket
+      client = net.createConnection(files.sock)
+      await connected(client)
+    } catch (error) {
+      // seems like no server is running, spawn a new server
+      this.emit('starting')
+      let command = process.env.DWMM_TEST ? 'babel-node' : process.execPath
+      if (!/node$/.test(command)) command = 'node'
+      const server = spawn(
+        command,
+        [require.resolve('./Server'), this.projectRoot],
+        {
+          detached: true,
+          stdio: 'ignore',
+          cwd: this.projectRoot,
+        }
+      )
+
+      client = await Promise.race([
+        // keep trying to connect to the socket in case the server is
+        // slow to start up
+        poll(async (): Promise<net.Socket> => {
+          const client = net.createConnection(files.sock)
+          await connected(client)
+          return client
+        }, 1000).timeout(30000),
+        // fail immediately if the server process exits
+        emitted(server, '', { rejectionEvents: ['exit', 'error'] }),
+      ])
+    }
+
+    client.pipe(this.instream)
+    this.outstream.pipe(client)
+
+    this.instream.on(
+      'data',
+      (message: {
+        seq?: number,
+        progress?: Progress,
+        ready?: boolean,
+        error?: string,
+      }) => {
+        const { seq, progress, ready, error } = message
+        if (seq != null) {
+          const callbacks = this.callbacks.get(seq)
+          if (callbacks) {
+            this.callbacks.delete(seq)
+            if (error) callbacks.reject(new Error(error))
+            else callbacks.resolve(message)
+          }
+        }
+        if (progress) this.emit('progress', progress)
+        if (ready) this.emit('ready')
+      }
+    )
+
+    client.once('error', () => {
+      this.client = null
+    })
+
+    return client
+  }
+
   async connect({
     startServer,
   }: {
     startServer?: ?boolean,
   } = {}): Promise<net.Socket> {
-    const actuallyConnect = async (): Promise<net.Socket> => {
-      const files = tempFiles(this.projectRoot)
-
-      const client = await new Promise(
-        (
-          resolve: (client: net.Socket) => any,
-          reject: (error: Error) => any
-        ) => {
-          let client,
-            child,
-            rejected = false
-
-          const cleanup = () => {
-            if (client != null) {
-              client.removeListener('error', handleInitialError)
-              client.removeListener('error', handleFinalError)
-            }
-            if (child != null) {
-              child.removeListener('error', handleFinalError)
-              child.removeListener('exit', handleExit)
-            }
-          }
-
-          const handleExit = (code: ?number, signal: ?string) => {
-            rejected = true
-            cleanup()
-            reject(
-              new Error(
-                code
-                  ? `process exited with code ${code}`
-                  : signal
-                  ? `process was killed with ${signal}`
-                  : `process exited`
-              )
-            )
-          }
-
-          const handleFinalError = (error: Error) => {
-            rejected = true
-            cleanup()
-            reject(error)
-          }
-
-          const handleConnect = () => {
-            cleanup()
-            resolve(client)
-          }
-          const handleInitialError = async (error: Error): Promise<void> => {
-            if (!startServer) {
-              reject(error)
-              return
-            }
-            this.emit('starting')
-            let command = process.env.DWMM_TEST
-              ? 'babel-node'
-              : process.execPath
-            if (!/node$/.test(command)) command = 'node'
-            child = spawn(
-              command,
-              [require.resolve('./Server'), this.projectRoot],
-              {
-                detached: true,
-                stdio: 'ignore',
-                cwd: this.projectRoot,
-              }
-            )
-            child.on('error', handleFinalError)
-            child.on('exit', handleExit)
-            const startTime = Date.now()
-            while (
-              !rejected &&
-              !(await fs.pathExists(files.sock).catch(() => false))
-            ) {
-              if (Date.now() - startTime > 10000) break
-              await delay(500)
-            }
-            if (!rejected) {
-              client = net.createConnection(files.sock, handleConnect)
-              client.on('error', handleFinalError)
-            }
-          }
-
-          client = net.createConnection(files.sock, handleConnect)
-          client.on('error', handleInitialError)
-        }
-      )
-
-      client.pipe(this.instream)
-      this.outstream.pipe(client)
-
-      this.instream.on(
-        'data',
-        (message: {
-          seq?: number,
-          progress?: Progress,
-          ready?: boolean,
-          error?: string,
-        }) => {
-          const { seq, progress, ready, error } = message
-          if (seq != null) {
-            const callbacks = this.callbacks.get(seq)
-            if (callbacks) {
-              this.callbacks.delete(seq)
-              if (error) callbacks.reject(new Error(error))
-              else callbacks.resolve(message)
-            }
-          }
-          if (progress) this.emit('progress', progress)
-          if (ready) this.emit('ready')
-        }
-      )
-
-      return client
-    }
-
     if (this.client) return await this.client
-    this.client = actuallyConnect()
-    const client = await this.client
-    client.once('error', () => {
-      this.client = null
-    })
-    return client
+    return await (this.client = this._createClient())
   }
 
   async request(message: $Diff<Message, { seq: number }>): Promise<any> {
